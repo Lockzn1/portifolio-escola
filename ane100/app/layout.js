b951fb87951c6c53784b9235f36761c5e3c0c345
Lockzn1 <cotrim.murilo@gmail.com>
2025-08-19 14:34:03 -0300

    vercel

--- ane100/app/layout.js
+++ ane100/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "BioTech Solutions",
+  title: " Ane100",
   description: "Startup fictícia focada em soluções para Anemia Falciforme",
 };
 
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
         {/* Navbar */}
         <header className="bg-[#2D728F] text-white shadow-md sticky top-0 z-50">
           <nav className="max-w-5xl mx-auto flex justify-between items-center py-4 px-6">
-            <h1 className="font-bold text-xl">BioTech</h1>
+            <h1 className="font-bold text-xl">Ane100</h1>
             <ul className="flex space-x-6">
               <li><a href="#genetica" className="hover:text-[#FFBFA0]">Genética</a></li>
               <li><a href="#sequenciamento" className="hover:text-[#FFBFA0]">Sequenciamento</a></li>
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
 
         {/* Rodapé */}
         <footer className="bg-[#77567A] text-white py-6 mt-12 text-center">
-          <p>© 2025 BioTech Solutions — Projeto acadêmico fictício</p>
+          <p>© 2025  Ane100 — Projeto acadêmico fictício</p>
         </footer>
       </body>
     </html>
